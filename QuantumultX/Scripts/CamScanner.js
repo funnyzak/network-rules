00fe4787fe10710531f3834439bf0def0a38fd8a
leon <silenceace@gmail.com>
2022-12-05 20:47:34 +0800

    chore: style

--- QuantumultX/Scripts/CamScanner.js
+++ QuantumultX/Scripts/CamScanner.js
@@ -1,15 +1,15 @@
 // 𝐅𝐫𝐨𝐦： https://raw.githubusercontent.com/id77/QuantumultX/master/Script/camscanner.js
 let body = JSON.parse($response.body);
 body.data.psnl_vip_property = {
-  renew_method: 'appstore',
-  initial_tm: '1614867690',
+  renew_method: "appstore",
+  initial_tm: "1614867690",
   svip: 1,
   auto_renewal: true,
   ms_first_pay: 0,
   pending: 0,
   group2_paid: 0,
   inherited_flag: 0,
-  nxt_renew_tm: '9915126887',
+  nxt_renew_tm: "9915126887",
   level_info: {
     level: 1,
     days: 1,
@@ -17,11 +17,11 @@ body.data.psnl_vip_property = {
   },
   group1_paid: 1,
   ys_first_pay: 0,
-  renew_type: 'year',
+  renew_type: "year",
   expiry: 8487890487,
   grade: 2,
-  last_payment_method: 'appstore',
-  product_id: 'com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low',
+  last_payment_method: "appstore",
+  product_id: "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low",
 };
 $done({
   body: JSON.stringify(body),
